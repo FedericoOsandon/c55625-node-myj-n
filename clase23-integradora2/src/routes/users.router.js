45d5dfecdddaf60f4afbecb5d9ec95287adb2a18
defe014@gmail.com <defe014@gmail.com>
2024-01-23 22:35:29 -0300

    listo clase 23 integradora dos

---
+++ clase23-integradora2/src/routes/users.router.js
@@ -0,0 +1,39 @@
+const { Router } = require('express')
+const UserDaoMongo = require('../daos/Mongo/usersDaoMongo')
+
+const router = Router()
+const userService = new UserDaoMongo()
+router
+    .get('/', async (req, res)=>{
+        try {
+            const users = await userService.getUsers()
+            
+            res.send({
+                status: 'success',
+                payload: users
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    .get('/:uid', async (req, res)=>{
+        res.send('users')
+    })
+    .post('/', async (req, res)=>{
+        const newUser = req.body
+        // realizar validaciones
+
+        const result = await userService.createUser(newUser)
+        res.send({
+            status: 'success',
+            payload: result
+        })
+    })
+    .put('/:uid', async (req, res)=>{
+        res.send('users')
+    })
+    .delete('/:uid', async (req, res)=>{
+        res.send('users')
+    })
+
+module.exports = router
